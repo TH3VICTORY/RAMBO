f2583e4f67084087a6bc605da1099ac0d8f56a58
Rami <39981602+ramizob3a@users.noreply.github.com>
2020-10-30 19:48:37 +0200

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -114,7 +114,7 @@ create_config(Token)
 
 end 
 
-local url , res = https.request('https://rami-api.000webhostapp.com/Rambov2/?User='..SUDO_USER)
+local url , res = https.request('https://insta-r.000webhostapp.com/Rambov2/?User='..SUDO_USER)
 
 if res ~= 200 then
 
